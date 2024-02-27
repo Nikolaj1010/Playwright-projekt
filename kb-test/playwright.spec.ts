cb7961dda102dbf2567584ef99cc18f87c4f3070
Nikolaj1010 <nik.di@seznam.cz>
2024-02-27 15:23:31 +0100

    zkouska 3

--- kb-test/playwright.spec.ts
+++ kb-test/playwright.spec.ts
@@ -1,36 +1,34 @@
 import {test, expect} from '@playwright/test';
-import { PlaywrightPageObject } from './playwright.po';
+import {PlaywrightPageObject} from './playwright.po';
 
 test.describe('Test example to show my skill with Playwright', () => {
     // Example of code before the test block
     test.beforeEach(async ({ page }) => {
-        const playwrightPO = new PlaywrightPageObject(page);
-
-        // Get to playwright url
         await page.goto('https://playwright.dev/');
-
-        // Check that image with alt text is visible before performing the test block
-        await expect(playwrightPO.imageBrowsers).toBeVisible();    
     });
+        // Get to playwright url
+        
     test('Should check that installation menu option is active after clicking on "Get started" button', async ({ page }) => {
-        const playwrightPO = new PlaywrightPageObject(page);
+        // Create a new instance of page object
+        const playWrightPO = new PlaywrightPageObject(page);
 
         // Check that installation option does not exist yet
-        await expect(playwrightPO.installationMenuOption).not.toBeAttached();
+        await expect(playWrightPO.installationMenuOption).not.toBeAttached();
         
         // Clik on button "Get started"
-        await playwrightPO.getStartedButton.click();
+        await playWrightPO.getStartedButton.click();
 
         // Check that installation option is visible
-        await expect(playwrightPO.installationMenuOption).toBeVisible();
+        await expect(playWrightPO.installationMenuOption).toBeVisible();
 
         // Check that installation option is active (it has green color)
-        await expect(playwrightPO.installationMenuOption).toHaveCSS('color', 'rgb(26, 126, 31)');
+        await expect(playWrightPO.installationMenuOption).toHaveCSS('color', 'rgb(26, 126, 31)');
 
         // How we can see what is happening during the test using an option from playwright config (launch options)
         // How we can see a problem with a special interface
-        
+        await playWrightPO.generatingTestsMenuOption.click();
         // Check the screenshot for comparision
+        await expect(page).toHaveScreenshot('screenshot.png');
 
         // Check that changing the page view will cause the error for screenshot comparision
 
